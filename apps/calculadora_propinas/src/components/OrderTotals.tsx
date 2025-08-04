bf50f95e5c705722733492fba6cf09550a0032c2
fguedez1311 <46085315+fguedez1311@users.noreply.github.com>
2025-08-04 15:56:31 -0400

    Finalizado proyecto de Calculadora de Propina

---
+++ apps/calculadora_propinas/src/components/OrderTotals.tsx
@@ -0,0 +1,43 @@
+import { useMemo } from "react"
+import type { OrderItem } from "../types"
+import { formatCurrency } from "../helpers"
+
+type OrderTotalsProp={
+    order:OrderItem[],
+    tip:number
+    placeOrder: () => void
+}
+
+export default function OrderTotals({order,tip,placeOrder}:OrderTotalsProp) {
+ const subTotalAmount=useMemo(()=>order.reduce((total,item)=>total+(item.quantity*item.price),0),[order])
+ const tipAmount=useMemo(()=>subTotalAmount*tip,[tip, order])
+ const totalAmount=useMemo(()=>subTotalAmount+tipAmount,[tip, order])
+  return (
+    <>
+        <div className="space-y-3">
+            <h2 className="font-black text-2xl">Totales y Propinas:</h2>
+            <p>
+                Subtotal a pagar:{' '} <span className="font-bold">{formatCurrency(subTotalAmount)}</span>
+
+
+            </p>
+            <p>
+                Propina:{' '} 
+                    <span className="font-bold">{formatCurrency(tipAmount)}</span>
+            </p>
+            <p>
+                Total a Pagar:{' '} 
+                    <span className="font-bold">{formatCurrency(totalAmount)}</span>
+            </p>
+            
+        </div>
+        <button
+            className="w-full bg-black p-3 uppercase text-white mt-10 disabled:opacity-10"
+            disabled={totalAmount===0}
+            onClick={placeOrder}
+        >
+            Guardar Order
+        </button>
+    </>
+  )
+}
---
+++ apps/calculadora_propinas/src/components/TipPercentageForm.tsx
@@ -0,0 +1,47 @@
+import type  { Dispatch,SetStateAction } from "react"
+const tipOptions = [
+  {
+    id: 'tip-10',
+    value: .10,
+    label: '10%'
+  },
+  {
+    id: 'tip-20',
+    value: .20,
+    label: '20%'
+  },
+  {
+    id: 'tip-50',
+    value: .50,
+    label: '50%'
+  },
+]
+type  TipPercentageFormPros={
+    setTip: Dispatch<SetStateAction<number>>
+    tip:number
+}
+
+
+export default function TipPercentageForm({setTip,tip}:TipPercentageFormPros) {
+  return (
+    <div>
+        <h3 className="font-black text-2xl">Propina: </h3>
+        <form>
+            {tipOptions.map(tipOption=>(
+                <div className="flex gap-2" key={tipOption.id}>
+                    <label htmlFor={tipOption.id}>{tipOption.label}</label>
+                    <input 
+                        id={tipOption.id}
+                        type="radio"
+                        name="tip"
+                        onChange={e=>setTip(+e.target.value)}
+                        value={tipOption.value}
+                        checked={tipOption.value===tip}
+                    />
+
+                </div>
+            ))}
+        </form>
+    </div>
+  )
+}
